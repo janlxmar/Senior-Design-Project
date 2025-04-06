c099817e1c033039b26dc67a146735a50c996d94
Jan Lemar Baltazar <158001377+janlxmar@users.noreply.github.com>
2025-04-06 13:17:35 -0700

    Update light.py

--- light.py
+++ light.py
@@ -4,12 +4,12 @@ import board
 from adafruit_mcp3xxx.mcp3008 import MCP3008
 from adafruit_mcp3xxx.analog_in import AnalogIn
 
-# Setup SPI and MCP3008
+
 spi = busio.SPI(clock=board.SCLK, MISO=board.MISO, MOSI=board.MOSI)
 cs = digitalio.DigitalInOut(board.D5)  # Or whatever pin you're using for CS
 mcp = MCP3008(spi, cs)
 
-# Create analog channel on CH3
+# channel threeeeeee
 chan = AnalogIn(mcp, 3)
 
 def read_light():
